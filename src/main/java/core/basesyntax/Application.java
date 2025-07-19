8b905779c401d479b11f2cdc13ad7e00e91cee22
Piotr Seredynski <sersoft14@gmail.com>
2025-07-19 07:10:45 +0200

    Finish project

--- src/main/java/core/basesyntax/Application.java
+++ src/main/java/core/basesyntax/Application.java
@@ -3,6 +3,9 @@ package core.basesyntax;
 public class Application {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
-        Ball ball = lottery.getRandomBall();
+        for (int i = 0; i < 3; i++) {
+            Ball ball = lottery.getRandomBall();
+            System.out.println("Ball number " + (i + 1));
+        }
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -7,9 +7,9 @@ public class Lottery {
 
     public Ball getRandomBall() {
         ColorSupplier colorSupplier = new ColorSupplier();
-        Color getRandomColor = colorSupplier.getRandomColor();
+        Color randomColor = colorSupplier.getRandomColor();
         int number = random.nextInt(100) + 1;
-        return new Ball(getRandomColor, number);
+        return new Ball(randomColor, number);
 
     }
 }
